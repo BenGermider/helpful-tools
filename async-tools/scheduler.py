e3365d37f41e71151371f4c86b61baa444fe5664
BenGermider <bengermidr@gmail.com>
2025-04-18 17:33:37 +0300

    Added scheduler stop

--- async-tools/scheduler.py
+++ async-tools/scheduler.py
@@ -4,11 +4,16 @@ class Scheduler:
 
     def __init__(self, timeout: int):
         self._timeout = timeout
+        self._running = True
 
     async def task(self):
         raise NotImplemented
 
+    async def stop(self):
+        self._running = False
+
     async def schedule(self):
-        while True:
+        while self._running:
             await self.task()
             await asyncio.sleep(self._timeout)
+
